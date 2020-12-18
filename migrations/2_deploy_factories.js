42b2d351667761130dd7c88190b56024f6114e1f
Bo Yao <bo@nearprotocol.com>
2020-12-18 11:58:58 -0800

    add betanet testnet mainnet migration

--- migrations/2_deploy_factories.js
+++ migrations/2_deploy_factories.js
@@ -3,7 +3,8 @@ const BToken = artifacts.require('BToken');
 const BFactory = artifacts.require('BFactory');
 
 module.exports = async function (deployer, network, accounts) {
-    if (network === 'development' || network === 'coverage' || network === 'near_local') {
+    if (network === 'development' || network === 'coverage' || network === 'near_local'
+        || network === 'near_betanet' || network === 'near_testnet' || network === 'near_mainnet') {
         deployer.deploy(TMath);
     }
     deployer.deploy(BFactory);
